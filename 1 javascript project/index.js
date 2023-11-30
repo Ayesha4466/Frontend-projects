7845393fe2df03bbeab559fc33f436667fb483e4
Ayesha Sadiqa <151496372+Ayesha4466@users.noreply.github.com>
2023-11-30 12:08:17 +0500

    Add files via upload

---
+++ 1 javascript project/index.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountemail = "ayesha@gmail.com"
+var accountpassword = "12345"
+accountcity = "lahore"
+// accountId = 2
+accountemail = "amna@gmail.com"
+accountpassword = "22345"
+accountcity = "okara"
+console.log(accountId);
+console.table([accountId ,accountemail, accountpassword , accountcity])
